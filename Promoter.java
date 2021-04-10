49ba1d51f18c6d699546e05c8eb14c363c71d97e
Patrick Witter <pwitterop123@gmail.com>
2021-04-10 18:16:09 -0500

    Modified Promoter class to include functions that will allow the value of 'nextId' to be retrieved and setted

--- Promoter.java
+++ Promoter.java
@@ -13,6 +13,7 @@ public class Promoter  implements Comparable<Promoter> {
 	private ArrayList<Venue> venues;
 	private ArrayList<Event> approvedEvents = new ArrayList<Event>();
 	private ArrayList<Plan> plannedEvents = new ArrayList<Plan>();
+
 	
 	public Promoter (String name, double budget, Ministry min, ArrayList<Venue> venues) {
 
@@ -23,6 +24,34 @@ public class Promoter  implements Comparable<Promoter> {
 		id = nextid;
 		nextid++;
   	}
+  	// This constructor is used when a Promoter is being created when it is read from file.
+	// Therefore it already has its own id
+	public Promoter (String name, double budget, Ministry min, ArrayList<Venue> venues, int Id) {
+
+		this.name = name;
+		this.budget = budget;
+		this.min = min;
+		this.venues= venues;
+		this.id = Id;
+
+	}
+
+	// This is called when the promoter is being written to file
+	// The value is written to the file that stores the next id.
+	public static int getNextId()
+	{
+		return nextid;
+	}
+
+	// If the promoter file is not empty this means that promoters
+	// have already been created. Therefore to prevent the program from
+	// creating new promoters from ids starting from 0. The last id that was saved is
+	// read from file and incremented by one (the parameter of this file) and this function
+	// is called with that value.
+	public static void setNextid(int id)
+	{
+		nextid = id;
+	}
 	//////
 	@Override
 	public int compareTo(Promoter other)
